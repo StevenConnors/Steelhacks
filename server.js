54ba0f9ce015128800c052db02220b0ccfa2592e
Steven C. Connors <scconnor@andrew.cmu.edu>
2016-02-23 13:48:45 -0500

    Cleaned code & deleted extraneous files.

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
-/*
- *  Node/Mongo server
- *
- *
- */
-
-
 var express = require('express');
 var bodyParser = require('body-parser');
 
-// Logging
 var morgan = require('morgan');
 
 var app = express();
@@ -16,8 +8,8 @@ var app = express();
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
-	    extended: true
-	}));
+    extended: true
+}));
 
 // Log all requests
 app.use(morgan("dev"));
@@ -25,10 +17,5 @@ app.use(morgan("dev"));
 
 app.use('/',express.static(__dirname + '/public'));
 app.use('/assets',express.static(__dirname + '/assets'));
-//app.use('/dist',express.static(__dirname + '../../../dist'));
-//app.use('/libs',express.static(__dirname + '../../../libs'));
 
-// Prevent, 304 (Not changed) for api
-//app.disable('etag');
-
-app.listen(10080);
+app.listen(8080);
